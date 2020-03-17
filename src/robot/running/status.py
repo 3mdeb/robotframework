82cb48751061801f8454a8250aaa59410f734c0c
Lajzer <Piotrkonkol01@gmail.com>
2020-03-17 17:44:25 +0100

    Changed func unicode to str to restore skip compatibility with python3

--- src/robot/running/status.py
+++ src/robot/running/status.py
@@ -150,7 +150,7 @@ class TestStatus(_ExecutionStatus):
         self.exit.failure_occurred(failure, self._test.critical)
 
     def test_skipped(self, skip, critical):
-        self.failure.skip = unicode(skip)
+        self.failure.skip = str(skip)
 
     def _my_message(self):
         return TestMessage(self).message
