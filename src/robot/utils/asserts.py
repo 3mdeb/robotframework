bf1ac0643af7931531068f5772e454f38d730810
Filip Gołaś <filip.golas@3mdeb.com>
2025-08-01 14:47:21 +0200

    utils/asserts.py: Add fuzzy comparisons
    Signed-off-by: Filip Gołaś <filip.golas@3mdeb.com>

--- src/robot/utils/asserts.py
+++ src/robot/utils/asserts.py
@@ -97,6 +97,7 @@ Example output::
 from .robottypes import type_name
 from .unic import safe_str
 
+import robot.utils.fuzzy as fuzzy
 
 def fail(msg=None):
     """Fail test immediately with the given message."""
@@ -178,6 +179,19 @@ def assert_equal(first, second, msg=None, values=True, formatter=safe_str):
     """Fail if given objects are unequal as determined by the '==' operator."""
     if not first == second:  # noqa: SIM201
         _report_inequality(first, second, "!=", msg, values, formatter)
+
+def assert_equal_fuzzy(first, second, msg=None, values=True, formatter=safe_str, percent_match=90, max_errors=None):
+    """Fail if given objects are unequal as determined by fuzzy comparison. Default is 90% similarity."""
+    match = fuzzy._fuzzy_find(first, second, percent_match, max_errors)
+    if not len(match) > 0:
+        _report_inequality(first, second, '!=', msg, values, formatter)
+
+def assert_not_equal_fuzzy(first, second, msg=None, values=True, formatter=safe_str, percent_match=90, max_errors=None):
+    """Fail if given objects are equal as determined by fuzzy comparison. Default is 90% similarity."""
+    match = fuzzy._fuzzy_find(first, second, percent_match, max_errors)
+    if len(match) > 0:
+        _report_inequality(first, second, '!=', msg, values, formatter)
+
 
 
 def assert_not_equal(first, second, msg=None, values=True, formatter=safe_str):
