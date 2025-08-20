16264f78f2e7bb25a376f0090796c301a5ebbc7e
Filip Gołaś <filip.golas@3mdeb.com>
2025-08-20 09:03:59 +0200

    fuzzy.py: Add robust type checks
    Signed-off-by: Filip Gołaś <filip.golas@3mdeb.com>

--- src/robot/utils/fuzzy.py
+++ src/robot/utils/fuzzy.py
@@ -10,21 +10,38 @@ def fuzzy_find(buffer, expected, percent_match=None, max_errors=None, max_insert
     return None
 
 def fuzzy_find_all(buffer, expected, percent_match:float=None, max_errors:int=None, max_insertions:int=None, max_deletions:int=None, ignore_case:bool=False):
-    
-    if max_insertions:
-        max_insertions=int(max_insertions)
-    if max_deletions:
-        max_deletions=int(max_deletions)
+    max_l_dist = 0
+
+    if max_insertions is not None:
+        try:
+            max_insertions=int(max_insertions)
+        except:
+            logger.warn(f"max_insertions parameter invalid: {max_insertions}:{type(max_insertions)}")
+            max_insertions=None
+
+    if max_deletions is not None:
+        try:
+            max_deletions=int(max_deletions)
+        except:
+            logger.warn(f"max_deletions parameter invalid: {max_deletions}:{type(max_deletions)}")
+            max_deletions=None
+
     if max_errors is not None:
-        max_errors = int(max_errors)
-        max_l_dist = max_errors
+        try:
+            max_errors = int(max_errors)
+            max_l_dist = max_errors
+        except:
+            logger.warn(f"max_errors parameter invalid: {max_errors}:{type(max_errors)}")
+            max_errors=None
     elif percent_match is not None:
-        percent_match = float(percent_match)
-        percent_match = min(100, max(0, percent_match)) # limit to 0-100
-        percent_errors = 100 - percent_match
-        max_l_dist = round(len(expected) * percent_errors / 100.0)
-    else:
-        max_l_dist = None
+        try:
+            percent_match = float(percent_match)
+            percent_match = min(100, max(0, percent_match)) # limit to 0-100
+            percent_errors = 100 - percent_match
+            max_l_dist = int(round(len(expected) * percent_errors / 100.0))
+        except:
+            logger.warn(f"percent_match parameter invalid: {percent_match}:{type(percent_match)}")
+            percent_match=None
     
     print("percent_match: ", percent_match, ": ", type(percent_match))
     print("max_errors: ", max_errors, ": ", type(max_errors))
